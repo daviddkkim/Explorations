a06722bd8c53568230a5f5b362ec25e0b6533663
david kim <david@readyset.io>
2023-02-16 14:40:26 -0500

    clean up and add cdn

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,12 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
 import { styled } from 'stitches.config'
-import { Table } from '@geist-ui/core'
 import { animate, spring, timeline } from "motion"
-import React, { useEffect, useLayoutEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { BatteryIcon, WifiIcon, SpinnerIcon } from '@iconicicons/react'
+import createGlobe from "cobe";
 
 
-const inter = Inter({ subsets: ['latin'] })
 
 const MainBlock = styled("div", {
   height: '100vh',
@@ -115,6 +113,50 @@ export default function Home() {
     setTimeout(() => { setButtonDisabled(false) }, time)
 
   }
+
+
+
+  const canvasRef = useRef(null);
+
+  useEffect(() => {
+    let phi = 0;
+    if (!canvasRef.current) return;
+
+    const globe = createGlobe(canvasRef.current, {
+      devicePixelRatio: 2,
+      width: 600 * 2,
+      height: 600 * 2,
+      phi: 0,
+      theta: 0,
+      dark: 1,
+      diffuse: 1.2,
+      mapSamples: 16000,
+      mapBrightness: 6,
+      baseColor: [0.3, 0.3, 0.3],
+      markerColor: [0.1, 0.8, 1],
+      glowColor: [1, 1, 1],
+      markers: [
+        // longitude latitude
+        { location: [37.7595, -122.4367], size: 0.03 },
+        { location: [40.7128, -74.006], size: 0.1 }
+      ],
+      onRender: (state) => {
+        // Called on every animation frame.
+        // `state` will be an empty object, return updated params.
+        state.phi = phi;
+        phi += 0.005;
+      }
+    });
+
+    return () => {
+      globe.destroy();
+    };
+  }, []);
+
+
+
+
+
 
   useEffect(() => {
     if (buttonDisabled) {
@@ -606,6 +648,17 @@ export default function Home() {
             </Box>
           </Box>
         </Box>
+      </MainBlock>
+      <MainBlock css={{
+        flexDirection: 'column'
+      }}>
+        <h1>
+          Global CDN for your database
+        </h1>
+        <canvas
+          ref={canvasRef}
+          style={{ width: 600, height: 600, maxWidth: "100%", aspectRatio: 1 }}
+        />
       </MainBlock>
     </>
   )
